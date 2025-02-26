d6396836ee82637dc1aa18d136f4cfbb215b31dc
didine712 <aguenassia@gmail.com>
2025-02-26 09:34:40 +0100

    test constructeur SalarieAideADomicile

--- src/test/java/com/ipi/jva350/SalarieAideADomicileTest.java
+++ src/test/java/com/ipi/jva350/SalarieAideADomicileTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import java.time.LocalDate;
 import java.util.LinkedHashSet;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -42,7 +43,30 @@ public class SalarieAideADomicileTest {
         salarie.setJoursTravaillesAnneeNMoins1(10);
         assertFalse(salarie.aLegalementDroitADesCongesPayes());
     }
-
+    @Test
+    void testConstructeur_SalarieAideADomicile() {
+        String nom = "Dupont";
+        LocalDate moisDebutContrat = LocalDate.of(2020, 1, 1);
+        LocalDate moisEnCours = LocalDate.of(2024, 2, 1);
+        double joursTravaillesAnneeN = 150;
+        double congesPayesAcquisAnneeN = 10;
+        double joursTravaillesAnneeNMoins1 = 200;
+        double congesPayesAcquisAnneeNMoins1 = 20;
+        double congesPayesPrisAnneeNMoins1 = 5;
+        SalarieAideADomicile salarie = new SalarieAideADomicile(
+                nom, moisDebutContrat, moisEnCours,
+                joursTravaillesAnneeN, congesPayesAcquisAnneeN,
+                joursTravaillesAnneeNMoins1, congesPayesAcquisAnneeNMoins1, congesPayesPrisAnneeNMoins1
+        );
+        assertThat(salarie.getNom()).isEqualTo(nom);
+        assertThat(salarie.getMoisDebutContrat()).isEqualTo(moisDebutContrat);
+        assertThat(salarie.getMoisEnCours()).isEqualTo(moisEnCours);
+        assertThat(salarie.getJoursTravaillesAnneeN()).isEqualTo(joursTravaillesAnneeN);
+        assertThat(salarie.getCongesPayesAcquisAnneeN()).isEqualTo(congesPayesAcquisAnneeN);
+        assertThat(salarie.getJoursTravaillesAnneeNMoins1()).isEqualTo(joursTravaillesAnneeNMoins1);
+        assertThat(salarie.getCongesPayesAcquisAnneeNMoins1()).isEqualTo(congesPayesAcquisAnneeNMoins1);
+        assertThat(salarie.getCongesPayesPrisAnneeNMoins1()).isEqualTo(congesPayesPrisAnneeNMoins1);
+    }
     @Test
     void testALegalementDroitADesCongesPayes_CasSansDroit() {
         SalarieAideADomicile salarie = new SalarieAideADomicile();
